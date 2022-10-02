7bfcc3cacc5d6c2446a070736f3aa5996b625fcf
Karlis <karlischan@gmail.com>
2022-10-02 17:48:51 -0400

    update project screenshot fileanmes

--- app/controllers/projects.server.controller.js
+++ app/controllers/projects.server.controller.js
@@ -8,9 +8,9 @@ exports.render = function(req, res) {
     res.render('projects', {
         title: "Kam's Portfolio",
         page: "Projects", 
-        projectImg1: "img/project1.png",
-        projectImg2: "img/project2.png",
-        projectImg3: "img/project3.png",
+        projectImg1: "img/Project1.png",
+        projectImg2: "img/Project2.png",
+        projectImg3: "img/Project3.png",
         projectAlt1: "Screenshot of ongoing education application: Nexxus",
         projectAlt2: "Screenshot of Real Estate Website Demo",
         projectAlt3: "Screenshot of Mobile Services Website Demo"
